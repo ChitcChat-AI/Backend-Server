75f8cbc3595816968f93794c20b5108feee8a8ef
Peer <peerfichman@gmail.com>
2024-04-28 16:40:43 +0300

    web socket configuration check

--- WebSocket.js
+++ WebSocket.js
@@ -1,16 +1,12 @@
  const {WebSocketServer, WebSocket} = require( 'ws');
 
-const wsExperiments = new WebSocketServer({port: process.env.PORT_WS_EXP, path: '/experiments'});
-const wsSurvey = new WebSocketServer({port: process.env.PORT_WS_SUR, path: '/survey'});
+const wsExperiments = new WebSocketServer({port: process.env.PORT_WS_EXP});
 
 wsExperiments.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
 });
-wsSurvey.on('connection', function connection(ws) {
-    ws.on('error', console.error);
 
-});
  wsExperiments.on('update_experiment', (newExperiment) => {
          wsExperiments.clients.forEach((client) => {
              if (client.readyState === WebSocket.OPEN)
@@ -18,11 +14,6 @@ wsSurvey.on('connection', function connection(ws) {
          })
      }
  )
- wsSurvey.on('update_survey_vote', (newSurveyResults) => {
-     wsSurvey.clients.forEach((client) => {
-         if (client.readyState === WebSocket.OPEN)
-             client.send(JSON.stringify(newSurveyResults));
-     })
- })
 
-module.exports = {wsExperiments, wsSurvey};
+
+module.exports = {wsExperiments};
