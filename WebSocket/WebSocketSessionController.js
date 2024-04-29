2506def2b28784cadc21658c1d51d024a1c2f670
Peer <peerfichman@gmail.com>
2024-04-29 16:32:46 +0300

    bug fix

--- WebSocket/WebSocketSessionController.js
+++ WebSocket/WebSocketSessionController.js
@@ -1,12 +1,12 @@
 const uuid = require("uuid");
-const { ExpClientMap } =require('../DB/Maps')
+const { ExpClientMap, WsClientMap} =require('../DB/Maps')
 const {getExperimentById} = require("../DB/Queries");
 const createSession =  async (req, res) => {
     const id= uuid.v4();
     const {exp_id} = req.body;
     req.session.userId = id;
     ExpClientMap.add(exp_id, id);
-    const {exp_status} = await getExperimentById(exp_id)
+    const {exp_status} = await getExperimentById(exp_id);
     res.status(200).json({exp_status : exp_status});
 
 }
