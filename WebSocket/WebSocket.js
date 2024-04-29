ac9bb0729f2e40a75e9cc258e0dd299fe1115396
Peer <peerfichman@gmail.com>
2024-04-29 16:28:03 +0300

    adding ws

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -46,20 +46,11 @@ server.on('upgrade', function (request, socket, head) {
 wss.on('connection',  async (ws, request) => {
     const userId = request.session.userId;
 
-
-
     ws.on('error', console.error);
     ws.on('exp-update', function (newExp) {
         const {exp_status} = newExp;
         ws.send({exp_status: exp_status});
     });
-
-    const expIds = ExpClientMap.getKeysByValue(userId)
-    expIds.map(async (id) => {
-        const {exp_status} = await getExperimentById(id);
-        ws.send({exp_status: exp_status});
-    })
-
     WsClientMap.add(userId, ws);
 
 });
--- WebSocket/WebSocketSessionController.js
+++ WebSocket/WebSocketSessionController.js
@@ -1,11 +1,13 @@
 const uuid = require("uuid");
 const { ExpClientMap } =require('../DB/Maps')
-const createSession =  (req, res) => {
+const {getExperimentById} = require("../DB/Queries");
+const createSession =  async (req, res) => {
     const id= uuid.v4();
     const {exp_id} = req.body;
     req.session.userId = id;
     ExpClientMap.add(exp_id, id);
-    res.status(200).send(`success registered client to ${exp_id}` );
+    const {exp_status} = await getExperimentById(exp_id)
+    res.status(200).json({exp_status : exp_status});
 
 }
 const destroySession =  (request, response)  =>{
