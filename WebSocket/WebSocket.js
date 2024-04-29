cd981e82f3daeb23d6d59a0cb3db097c51d7ba3f
Peer <peerfichman@gmail.com>
2024-04-29 19:10:52 +0300

    sending back exp status on connection

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -61,19 +61,21 @@
 // });
 
 
-const { WebSocketServer } =  require ( 'ws');
-const {WsClientMap,ExpClientMap} = require('../DB/Maps')
+const {WebSocketServer} = require('ws');
+const {WsClientMap, ExpClientMap} = require('../DB/Maps')
 const uuid = require("uuid");
 const {json} = require("express");
-const wss = new WebSocketServer({ port: 3001 });
+const {getExperimentById} = require("../DB/Queries");
+const wss = new WebSocketServer({port: 3001});
 
-wss.on('connection',(ws) => {
+wss.on('connection',  (ws) => {
     const clientId = uuid.v4();
     ws.on('error', console.error);
-    ws.on('message', (data)=> {
-        console.log(data)
-        const msg =  JSON.parse(data);
-        ExpClientMap.add(clientId,msg.data)
+    ws.on('message', async (data) => {
+        const msg = JSON.parse(data);
+        ExpClientMap.add(clientId, msg.data);
+        const {exp_status} = await getExperimentById(msg.data);
+        ws.send(JSON.stringify({exp_status: exp_status}))
     });
     ws.on('exp-update', (newExp) => {
         const {exp_status} = newExp;
