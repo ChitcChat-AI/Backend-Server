921e3d41bb25b4aee95219cc1e4d6ff27b03f9ab
Peer <peerfichman@gmail.com>
2024-05-13 15:12:49 +0300

    study id and name added to query get experiment with agents by id

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
+app.
 app.use('/api/studies', studiesRouter);
 app.use('/api/experiments', experimentsRouter);
 app.use('/api/agents', agentsRouter);
