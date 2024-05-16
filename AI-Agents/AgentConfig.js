5e701893429cc78a4ff2ef0de1abbadc1d3956a3
DorHaimovich <Dorhaim765@gmail.com>
2024-05-16 09:45:45 +0300

    hotfix/ removed topics of intrest from agentprompt

--- AI-Agents/AgentConfig.js
+++ AI-Agents/AgentConfig.js
@@ -1,15 +1,12 @@
 const generateAgentPrompt = (agent, subject) => {
+  const { sentiment, opinion_alignment, talking_style, topics_of_interest } =
+    agent;
+  return {
+    role: "system",
+    content:
+      `You are human participating in a discussion about ${subject}. You ${opinion_alignment} the subject of discussion.` +
+      `Answer in ${talking_style} talking style, with ${sentiment} sentiment. Respond like a human with maximum two sentences. Sometimes add common typos. Don't use punctuation`,
+  };
+};
 
-    const {sentiment, opinion_alignment, talking_style, topics_of_interest} = agent;
-    return {
-        role: 'system',
-        content: `You are human participating in a discussion about ${subject}. You ${opinion_alignment} the subject of discussion.`
-            + (topics_of_interest.length > 0 ? `your personal topics of interest are: ` + topics_of_interest.reduce((acc, curr) => {
-                return acc + ', ' + curr
-            }, "") + `. ` : "")
-            + `Answer in ${talking_style} talking style, with ${sentiment} sentiment. Respond like a human with maximum two sentences. Sometimes add common typos. Don't use punctuation`
-    }
-
-}
-
-module.exports = {generateAgentPrompt};
+module.exports = { generateAgentPrompt };
