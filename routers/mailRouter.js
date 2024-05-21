3ccb25540dea014b33445aa0ba9c5b704f23774b
DorHaimovich <Dorhaim765@gmail.com>
2024-05-21 14:21:47 +0300

    chore: Refactor mailRouter.js for consistency and readability

--- routers/mailRouter.js
+++ routers/mailRouter.js
@@ -1,22 +1,26 @@
-const {Router} = require('express');
-const {addParticipantToExperiment} = require('../DB/Queries')
-const {sendMailToParticipant} = require('../BuissnessLogic/SendMailToParticipants')
+const { Router } = require("express");
+const { addParticipantToExperiment } = require("../DB/Queries");
+const {
+  sendMailToParticipant,
+} = require("../BuissnessLogic/SendMailToParticipants");
 const queries = require("../DB/Queries");
-const {APIError} = require("../ErrorHaneling/APIError");
-const {registerOrJoin} = require('../constants')
+const { APIError } = require("../ErrorHaneling/APIError");
+const { registerOrJoin } = require("../constants");
 const mailRouter = new Router();
 
-mailRouter.post('/', async (req, res, next) => {
-    try {
-        const {exp_id, user_id} = req.body;
-        if (!exp_id || !user_id) throw new Error('exp_id and user_id required in request body!');
-        await addParticipantToExperiment(exp_id, user_id)
-        await sendMailToParticipant(exp_id, user_id,registerOrJoin.REGISTER );
-        res.status(200).send("Successfully registered participant to experiment");
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
+mailRouter.post("/", async (req, res, next) => {
+  try {
+    const { exp_id, user_id } = req.body;
+    if (!exp_id || !user_id)
+      throw new Error("exp_id and user_id required in request body!");
+    console.log("body: ", exp_id, user_id);
+    await addParticipantToExperiment(exp_id, user_id);
+    await sendMailToParticipant(exp_id, user_id, registerOrJoin.REGISTER);
+    res.status(200).send("Successfully registered participant to experiment");
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
 });
 
-module.exports = {mailRouter};
+module.exports = { mailRouter };
