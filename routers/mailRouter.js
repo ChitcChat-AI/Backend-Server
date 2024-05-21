8b7bf91fe052704efc20d243ed32f613ce1194fd
Peer <peerfichman@gmail.com>
2024-05-21 09:58:41 +0300

    bug fixes

--- routers/mailRouter.js
+++ routers/mailRouter.js
@@ -3,6 +3,7 @@ const {addParticipantToExperiment} = require('../DB/Queries')
 const {sendMailToParticipant} = require('../BuissnessLogic/SendMailToParticipants')
 const queries = require("../DB/Queries");
 const {APIError} = require("../ErrorHaneling/APIError");
+const {registerOrJoin} = require('../constants')
 const mailRouter = new Router();
 
 mailRouter.post('/', async (req, res, next) => {
@@ -10,7 +11,7 @@ mailRouter.post('/', async (req, res, next) => {
         const {exp_id, user_id} = req.body;
         if (!exp_id || !user_id) throw new Error('exp_id and user_id required in request body!');
         await addParticipantToExperiment(exp_id, user_id)
-        await sendMailToParticipant(exp_id, user_id);
+        await sendMailToParticipant(exp_id, user_id,registerOrJoin.REGISTER );
         res.status(200).send("Successfully registered participant to experiment");
     } catch (err) {
         const apiError = new APIError(err)
