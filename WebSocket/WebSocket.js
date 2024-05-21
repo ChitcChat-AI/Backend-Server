3dbf68c018e8307e3e15adb297e3f2a037f1ae0b
DorHaimovich <Dorhaim765@gmail.com>
2024-05-21 14:33:40 +0300

    chore: Refactor WebSocket.js for consistency and readability

--- WebSocket/WebSocket.js
+++ WebSocket/WebSocket.js
@@ -60,34 +60,31 @@
 //     console.log('WS server is listening on  port 3001');
 // });
 
-const {WebSocketServer} = require('ws');
-const {WsClientMap, ExpClientMap} = require('../DB/Maps')
+const { WebSocketServer } = require("ws");
+const { WsClientMap, ExpClientMap } = require("../DB/Maps");
 const uuid = require("uuid");
-const {getExperimentById} = require("../DB/Queries");
-const {handleError} = require('../ErrorHaneling/APIError')
-const wss = new WebSocketServer({port: 3001});
+const { getExperimentById } = require("../DB/Queries");
+const { handleError } = require("../ErrorHaneling/APIError");
+const wss = new WebSocketServer({ port: 3001 });
 
-
-wss.on('connection',  (ws) => {
-    const clientId = uuid.v4();
-    WsClientMap.add(clientId, ws);
-    ws.on('error', async (err) => await handleError(err));
-    ws.on('message', async (data) => {
-        const msg = JSON.parse(data);
-        ExpClientMap.add(clientId, msg.data);
-        const {exp_status} = await getExperimentById(msg.data);
-        ws.send(JSON.stringify({exp_status: exp_status}))
-    });
-    ws.on('exp-update', (newExp) => {
-        const {exp_status} = newExp;
-        ws.send(JSON.stringify({exp_status: exp_status}));
-    })
-    ws.on('close', ()=>{
-        ExpClientMap.remove(clientId);
-        WsClientMap.get(clientId).terminate();
-        WsClientMap.remove(clientId);
-
-    })
-
+wss.on("connection", (ws) => {
+  const clientId = uuid.v4();
+  WsClientMap.add(clientId, ws);
+  ws.send(JSON.stringify({ clientId: clientId }));
+  ws.on("error", async (err) => await handleError(err));
+  ws.on("message", async (data) => {
+    const msg = JSON.parse(data);
+    ExpClientMap.add(clientId, msg.data);
+    const { exp_status } = await getExperimentById(msg.data);
+    ws.send(JSON.stringify({ exp_status: exp_status }));
+  });
+  ws.on("exp-update", (newExp) => {
+    const { exp_status } = newExp;
+    ws.send(JSON.stringify({ exp_status: exp_status }));
+  });
+  ws.on("close", () => {
+    ExpClientMap.remove(clientId);
+    WsClientMap.get(clientId).terminate();
+    WsClientMap.remove(clientId);
+  });
 });
-
