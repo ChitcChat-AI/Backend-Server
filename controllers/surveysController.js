7191c0d54b013700746b0f1b352e60e6d4abec7c
DorHaimovich <Dorhaim765@gmail.com>
2024-05-21 14:02:34 +0300

    hotfix/survey conroller

--- controllers/surveysController.js
+++ controllers/surveysController.js
@@ -1,57 +1,65 @@
-const queries = require('../DB/Queries');
-const {wsSurvey} = require('../WebSocket/WebSocket')
-const {APIError} = require("../ErrorHaneling/APIError");
+const queries = require("../DB/Queries");
+const { wsSurvey } = require("../WebSocket/WebSocket");
+const { APIError } = require("../ErrorHaneling/APIError");
 const insertAnswerPre = async (req, res, next) => {
-    try {
-        const {exp_id, user_id, opinion_pre} = req.body;
-        res.status(200).json(await queries.addSurveyAnswerPre(exp_id, user_id, opinion_pre));
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
-}
+  try {
+    const { exp_id, user_id, opinion } = req.body;
+    res
+      .status(200)
+      .json(await queries.addSurveyAnswerPre(exp_id, user_id, opinion));
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
+};
 const insertAnswerPost = async (req, res, next) => {
-    try {
-        const {exp_id, user_id, opinion_post} = req.body;
-        res.status(200).json(await queries.addSurveyAnswerPost(exp_id, user_id, opinion_post));
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
-}
+  try {
+    const { exp_id, user_id, opinion } = req.body;
+    res
+      .status(200)
+      .json(await queries.addSurveyAnswerPost(exp_id, user_id, opinion));
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
+};
 
 const getSurveyStatsByExperimentId = async (req, res, next) => {
-    try {
-        const exp_id = req.params.id;
-        res.status(200).json(await queries.getSurveyStatsById(exp_id));
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
-}
+  try {
+    const exp_id = req.params.id;
+    res.status(200).json(await queries.getSurveyStatsById(exp_id));
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
+};
 
-const isParticipantAnsweredPre = async (req,res, next) => {
-    try {
-        const {exp_id, user_id} = req.body;
-        res.status(200).json(await queries.isParticipantAnswerSurveyPre(exp_id,user_id));
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
-}
-const isParticipantAnsweredPost = async (req,res, next) => {
-    try {
-        const {exp_id, user_id} = req.body;
-        res.status(200).json(await queries.isParticipantAnswerSurveyPost(exp_id,user_id));
-    } catch (err) {
-        const apiError = new APIError(err)
-        next(apiError, req, res);
-    }
-}
+const isParticipantAnsweredPre = async (req, res, next) => {
+  try {
+    const { exp_id, user_id } = req.body;
+    res
+      .status(200)
+      .json(await queries.isParticipantAnswerSurveyPre(exp_id, user_id));
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
+};
+const isParticipantAnsweredPost = async (req, res, next) => {
+  try {
+    const { exp_id, user_id } = req.body;
+    res
+      .status(200)
+      .json(await queries.isParticipantAnswerSurveyPost(exp_id, user_id));
+  } catch (err) {
+    const apiError = new APIError(err);
+    next(apiError, req, res);
+  }
+};
 module.exports = {
-    insertAnswerPost,
-    insertAnswerPre,
-    getSurveyStatsByExperimentId,
-    isParticipantAnsweredPre,
-    isParticipantAnsweredPost
-}
+  insertAnswerPost,
+  insertAnswerPre,
+  getSurveyStatsByExperimentId,
+  isParticipantAnsweredPre,
+  isParticipantAnsweredPost,
+};
