48cab7cc3d3a52150586b393718181cabaa269ad
DorHaimovich <Dorhaim765@gmail.com>
2024-05-21 16:10:44 +0300

    hotfix

--- BuissnessLogic/ChangeExperimentStatus.js
+++ BuissnessLogic/ChangeExperimentStatus.js
@@ -1,30 +1,26 @@
-const {ExpUnsubMap} = require('../DB/Maps');
-const {updateExperimentStatus} =require('../DB/Queries');
-const {statusOptions} =require('../constants');
-const {firestoreColListener} = require('../DB/FirebaseColListener');
-const {ExpClientMap ,WsClientMap} = require('../DB/Maps');
-const {sendMailToAllParticipants} =require('./SendMailToParticipants')
+const { ExpUnsubMap } = require("../DB/Maps");
+const { updateExperimentStatus } = require("../DB/Queries");
+const { statusOptions } = require("../constants");
+const { firestoreColListener } = require("../DB/FirebaseColListener");
+const { ExpClientMap, WsClientMap } = require("../DB/Maps");
+const { sendMailToAllParticipants } = require("./SendMailToParticipants");
 const ChangeExperimentStatus = async (expId, newStatus) => {
+  if (newStatus === statusOptions.RUNNING) {
+    const unsub = firestoreColListener(expId);
+    ExpUnsubMap.add(expId, unsub);
+    await sendMailToAllParticipants(expId, "join");
+  }
+  if (newStatus === statusOptions.COMPLETED) {
+    ExpUnsubMap.get(expId);
+    ExpUnsubMap.remove(expId);
+  }
+  const newExp = await updateExperimentStatus(expId, newStatus);
+  const clientsArr = ExpClientMap.getKeysByValue(expId);
+  clientsArr.map((client) => {
+    const ws = WsClientMap.get(client);
+    if (ws) ws.emit("exp-update", newExp);
+  });
+  return newExp;
+};
 
-    if (newStatus === statusOptions.RUNNING){
-        const unsub = firestoreColListener(expId);
-        ExpUnsubMap.add(expId, unsub);
-        await sendMailToAllParticipants(expId,'join');
-
-    }
-    if (newStatus === statusOptions.COMPLETED){
-        ExpUnsubMap.get(expId)();
-        ExpUnsubMap.remove(expId);
-    }
-    const newExp = await updateExperimentStatus(expId, newStatus);
-    const clientsArr =  ExpClientMap.getKeysByValue(expId);
-    clientsArr.map((client) =>{
-        const ws = WsClientMap.get(client);
-        if(ws) ws.emit('exp-update', newExp);
-    })
-    return newExp;
-
-
-}
-
-module.exports = {ChangeExperimentStatus}
+module.exports = { ChangeExperimentStatus };
