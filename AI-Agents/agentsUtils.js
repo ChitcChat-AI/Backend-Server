5144c0c7506cca70362137d69e5645d62178b8ce
DorHaimovich12 <Dor.haimovich@stu.shenkar.ac.il>
2024-06-06 11:43:52 +0300

    chore: Update agent name in console logs to agent_name

--- AI-Agents/agentsUtils.js
+++ AI-Agents/agentsUtils.js
@@ -5,8 +5,17 @@ const determineWhichAgentToAnswer = (agents, exp) => {
   const TimeOut = 3000;
   for (const agent of agents) {
     const randNum = Math.round(Math.random() * maxRand);
+    console.log("Random number: " + randNum);
+    console.log("agent.agent_name: " + agent.agent_name);
 
     if (randNum <= agent.activity_level) {
+      console.log(
+        "Agent " +
+          agent.agent_name +
+          " is answering in: " +
+          TimeOut / 1000 +
+          " seconds"
+      );
       setTimeout(() => {
         agentHandler(agent, exp);
       }, TimeOut);
